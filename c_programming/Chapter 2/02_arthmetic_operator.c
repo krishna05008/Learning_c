7000293c86f0285080fb1442d27a4f48b5d44dba
Krishna Panjiyar <143351581+krishna05008@users.noreply.github.com>
2024-08-31 19:41:36 +0000

    chapter 2 done

---
+++ c_programming/Chapter 2/02_arthmetic_operator.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    int a = 4;
+    int b = 3;
+    int c = a+b;
+    printf("The value of a is %d and value of b is %d and sum is %d\n",a,b,c);
+    //Modulus operator is used to ger the remainder
+    printf("The remainder when a is divided by b is %d\n",a%b);
+
+    // this does not work for exponentiation is c
+    // int d a^b;
+
+
+    return 0;
+}
